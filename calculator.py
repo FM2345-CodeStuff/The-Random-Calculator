0562a66c0f8533e8f59c830f8569b3a871ac609a
FM2345-CodeStuff <163683893+FM2345-CodeStuff@users.noreply.github.com>
2024-03-16 17:55:42 -0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to Random Calculator")
+a = int(input('Select 1st number to add: '))
+b = int(input('Select 2nd number to add: '))
+
+sum_result = a + b
+
+print(f'Sum of {a} and {b} is {sum_result}')
